3ca314d035ddadb37a1302645edb4b086357e853
EliaGiolli <146369060+EliaGiolli@users.noreply.github.com>
2023-11-13 11:19:40 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+let container = document.createElement("div");
+div.setAttribute("class", "mainContainer");
+console.log(container);
+
+let numeroCounter = document.appendChild("label");
+label.setAttribute("id", "countLabel");
+let text = document.createTextNode("0");
+
+let bottoneDecremento = document.appendChild("button");
+button.setAttribute("id", "decreaseBtn");
+
+let bottoneReset = document.appendChild("button");
+button.setAttribute("id", "resetBtn");
+
+let bottoneAumento = document.appendChild("button");
+button.setAttribute("id", "increaseBtn");
